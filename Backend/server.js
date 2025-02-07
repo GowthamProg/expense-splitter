e67a5a7cbabfe1e9a088cbc20c87b0aecd2e07ea
Gowtham M S <gowthamshan1.12@gmail.com>
2025-02-07 11:53:57 +0530

    demo

--- Backend/server.js
+++ Backend/server.js
@@ -56,6 +56,107 @@ app.post('/Registor',async(req,res)=>{
     }
 });
 
+
+//token handling
+app.get('/validate-token',(req,res)=>{
+    const token= req.headers.authorization?.split(' ')[1];
+    if(!token)
+    {return res.status(401).json({message:"TOken missing"})};
+
+    try{
+        const decode =jwt.verify(token,Secret_key);
+        return res.status(200).json({valid:true,decode});
+    }catch(err){
+        return res.status(401).json({valid:false,message:"Invalid token"});
+    }
+});
+
+
+
+
+//handle to add frnds number
+app.post('/Members',async(req,res)=>{
+    const {username,frndname,frndnumber} = req.body;
+    try{
+        const usercollection=await connectToDatabase();
+        const userdataCollection =await connectTouserDatabase();
+
+        const user =await usercollection.findOne({username});
+        if(!user)
+        { return res.status(404).json({message:"User not found"});}
+
+        const update = await userdataCollection.updateOne(
+            {user_id :user._id},
+            {$push : {friends :{ frndname,frndnumber}}}
+        )
+        // await usercollection.insertOne({frndname,frndnumber});
+        res.status(201).json({message:"Reg sucessfull"});
+    }catch(error){
+        res.status(500).json({message:"Error", error});
+    }
+});
+
+
+// handle to show frnds details in friends page
+app.get('/Members/:username',async(req,res)=>{
+    const {username} =req.params;
+
+    try{
+        const usercollection = await connectToDatabase();
+        const userdataCollection =await connectTouserDatabase();
+
+        const user =await usercollection.findOne({username});
+        if(!user ) { 
+            return res.status(404).json({message:"User not found"}); }
+        
+        const userdata = await userdataCollection.findOne({user_id:user._id});
+        if(!userdata || !userdata.friends){
+            return res.status(200).json({friends : []});}
+        
+        res.status(200).json({friends : userdata.friends});
+    } catch(error){
+        res.status(500).json({message: "Error fetching data ",error});
+    }
+});
+
+
+//handle to update the friends in the member page
+app.put('/Members/:username/:frndname' ,async(req,res) =>{
+    const {username,frndname} =req.params;
+    const {updateFrndname , updateFrndNumber} = req.body;
+    try{
+        const userfrndcollection= await connectTouserDatabase();
+        const result = await userfrndcollection.updateOne(
+            {username,frndname},
+            {$set: {frndname :updateFrndname,frndnumber:updateFrndNumber}}
+        );
+        if(result.modifiedcount === 0)
+            return res.status(404).json({message:"Friend not found"});
+        res.status(200).json({message:"updated sucessfully"});
+    }catch(error){
+        res.status(500).json({message:"Error updating",error});
+    }
+});
+
+
+//handle to delete the friend in the member page
+app.delete('/Members/:username/:frndname',async(req,res)=>{
+    const {username , frndname} = req.params;
+    console.log(username,frndname); 
+try{
+    const userdatacollection = await connectTouserDatabase();
+    const result = userdatacollection.updateOne(
+        {username},
+        {$pull:{friends : {frndname}}}
+    );
+    
+    if(result.modifiedcount===0) 
+        return res.status(404).json({message:"Freind not found"});
+    res.status(200).json({message:"Deleted"});
+}catch(error){
+    res.status(500).json({message:"Error",error});
+}
+});
 const PORT= 5000;//process.env.PORT ||
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
---
+++ expense/src/Members.js
@@ -0,0 +1,143 @@
+import React,{ useEffect, useState } from 'react';
+import Sidebar from './Sidebar';
+import './Allstyles/Addmember.css';
+ function Members (){
+  const [appear,SetAppear] =useState(false);
+  const [editappear,SetEditappear] =useState(false);
+  const [hover,setHover]=useState(false);
+  const [editfrnd,setEditfrnd]=useState(null);
+
+  const [frndname,setFrndname] =useState('');
+  const [frndnumber, setFrndnumber]=useState('');
+  const [newfrndname,setNewfrndname]=useState('');
+  const [newfrndnumber,setNewfrndnumber]=useState('');
+  const [friends,setFriends] =useState([]);
+  const username = localStorage.getItem('username');
+
+
+  //fetch data from database
+  useEffect(()=>{
+    const fetchfriends =async () =>{
+      if(!username) return;
+      try{
+        const response=await fetch(`http://localhost:5000/Members/${username}`);
+        const data =await response.json();
+        if(response.ok)
+        {
+          console.log(" friends contact updated successfully");
+          setFriends(data.friends);
+        }
+        else alert("failed to fetch...");
+      }catch(error){
+          console.error("Error fetching",error);
+      }
+    };
+    fetchfriends();
+  },[username]);
+
+//add new friends 
+  const handlepost = async (e) =>{
+    e.preventDefault();
+    console.log({username,frndname,frndnumber});
+    const response =await fetch("http://localhost:5000/Members",{
+      method:"POST",
+      headers:{"content-Type" : "application/json"},
+      body : JSON.stringify({username,frndname,frndnumber})
+    });
+    if(response.ok)
+    {
+      alert("registor sucessfull");
+      window.location.reload();
+    }
+    else alert("registration failed");
+  };
+
+
+//delete the friends
+const handledeletefrnd = async(frndname) =>{
+  console.log(frndname);
+  const response =await fetch(`http://localhost:5000/Members/${username}/${frndname}`,{
+  method:"DELETE" 
+   });
+const data =response.json();
+if(response.ok)
+      {
+        alert("Deleted sucessfully",data.message);
+        setFriends(friends.filter(friend => friend.frndname !== frndname));
+      }
+else alert("Failed to delete",data.message);
+};
+
+//Editing the friend
+const handleEdit =(friends) =>{
+    SetEditappear(true);
+    console.log("Applied edit appear",editappear);
+    setEditfrnd(friends);
+    setNewfrndname(friends.frndname);
+    setNewfrndnumber(friends.frndnumber);
+};
+const handleupdate =async(e) =>{
+    e.preventDefault();
+    const response =await fetch(`http://localhost:5000/Members/${username}/${editfrnd}`,{
+      method : "Put" ,
+      headers : {"Content-Type" : "application/json"},
+      body :JSON.stringify({newfrndname,newfrndnumber}),
+    });
+    if(response.ok){
+      alert ("Freind updated sucessfully");
+      setFriends(friends.map
+        (friends => friends.frndname === editfrnd.frndname ? {frndname :newfrndname ,frndnumber: newfrndnumber} : friends ));
+      setEditfrnd(null);
+      }else alert("Failed to update");
+};
+
+
+
+  return (  
+    <>
+        < Sidebar />
+        <div className='content'>
+            <h2>Manage friends</h2>
+            <button className='createboxadd' onClick={()=>SetAppear(!appear)}> Add Number</button>
+            { appear &&(
+              <form className={`form ${appear ? 'active' : ''}`}>
+                <label>Name :</label>
+               <input type='text' placeholder='Name' onChange={(e)=>setFrndname(e.target.value)} required /><br/>
+               <label>Phone Number :</label>
+               <input type='number' placeholder='mobile no'  onChange={(e)=>setFrndnumber(e.target.value)} required/><br/>
+               <div className='form-buttons'>
+               <button className ="subbutton"onClick={handlepost}> Submit</button>
+               <button className ="clbutton" onClick={()=>SetAppear(false)}> close</button>
+               </div>
+             </form>
+            )}
+
+            <div className='friends-container'>
+                {friends.map((friend,index) => (
+                  <div  key={index} onClick={()=>setHover(!hover)} className='createboxadd'>      {/* //onClick={()=>handlehover(friend.frndname) */}
+                    {friend.frndname} <br/> {friend.frndnumber}<br/>
+                        {hover &&  (<div className='hover'>
+                            <button onClick={()=>handleEdit(friend)} className='edit-button'>Edit </button>
+                            <button onClick={()=>handledeletefrnd(friend.frndname)} className='edit-button'>delete </button>
+                        </div>)}
+                  </div>
+                ))}
+            </div>
+        {editappear && (
+          <form className={`form ${editappear ? 'active' : ''}`}>
+          <label> New Name :</label>
+         <input type='text' placeholder=' new Name' onChange={(e)=>setNewfrndname(e.target.value)} required /><br/>
+         <label>Phone Number :</label>
+         <input type='number' placeholder='new mobile no'  onChange={(e)=>setNewfrndnumber(e.target.value)} required/><br/>
+         <div className='form-buttons'>
+         <button className ="subbutton"onClick={handleupdate}> Submit</button>
+         <button className ="clbutton" onClick={()=>SetEditappear(false)}>cancel</button>
+         </div>
+       </form>
+        )}
+      </div>
+    </>
+  )
+}
+
+export default Members;
