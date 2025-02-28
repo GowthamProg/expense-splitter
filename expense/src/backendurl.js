45b35ae6d5b37759241500339f11f65b9b2fe060
Gowtham M S <gowthamshan1.12@gmail.com>
2025-02-28 14:07:04 +0530

    test 3

--- expense/src/backendurl.js
+++ expense/src/backendurl.js
@@ -1,3 +1,3 @@
 //export const url="http://192.168.137.1:5000";
 //export const url ="http://localhost:5000";
-export const url ="https://expense-splitter-lg7z.onrender.com"
+export const url ="https://expense-splitter-lg7z.onrender.com";
